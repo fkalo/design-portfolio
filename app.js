a05a3798e2af077e601bb05052d8ccb33f495cf8
Wepstern <balogh.gergo.mail@gmail.com>
2022-09-03 22:07:11 +0200

    zone page

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var zoneRouter = require('./routes/zone');
 
 var app = express();
 
@@ -22,6 +22,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/zone', zoneRouter);
 
 module.exports = app;
