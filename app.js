baacf687f3c6aa348dbb1773d8af0bdfbc1c50df
Wepstern <balogh.gergo.mail@gmail.com>
2022-09-03 21:39:05 +0200

    init npm project

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+let mustacheExpress = require('mustache-express');
+let bodyParser = require('body-parser');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.set('views', './views');
+app.set('view engine', 'mustache');
+app.engine('mustache', mustacheExpress());
+app.use (bodyParser.urlencoded( {extended : true} ) );
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
