e76a9c4edba72d829f840c5a4dd8a24fd89adcbc
Wepstern <balogh.gergo.mail@gmail.com>
2022-09-03 22:51:19 +0200

    add readme

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// IMPORT ROUTERS:
 var indexRouter = require('./routes/index');
 var zoneRouter = require('./routes/zone');
 
@@ -21,6 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// PAGE ROUTING:
 app.use('/', indexRouter);
 app.use('/zone', zoneRouter);
 
